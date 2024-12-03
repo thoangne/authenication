7fa10c7341e11dbb217936e0e847a7fb39ac7853
Thoangne <Nguythoang2003@gmail.com>
2024-12-03 23:44:46 +0700

    update

---
+++ authApp/form.py
@@ -0,0 +1,20 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput,label="Confirm password")
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm']
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get('password_confirm')
+        
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data
